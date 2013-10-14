d9fc70f604de211560de84a49101cba01ea97d85
johndgiese <johndgiese@gmail.com>
2013-10-13 20:58:31 -0400

    Add percentile option to center_dynamic_range

--- center_dynamic_range.m
+++ center_dynamic_range.m
@@ -1,8 +1,25 @@
-function centered_array = center_dynamic_range(array)
+function centered_array = center_dynamic_range(data, varargin)
 %CENTER_DYNAMIC_RANGE Fix original zero at 0.5 and then fill to [0, 1].
+%
+% If only one argument is provided, then the array is centered on the min and
+% max.  Otherwise, the optional second argument specifies the min and maximum
+% as a percentile.  For example to set the min value at the lowest 1 percentile
+% and the maximum value at the 99 percentile:
+%
+%     img_c = center_dynamic_range(img, 1);
+%
 
-    array_min = min(array(:));
-    array_max = max(array(:));
-    scale = 2.0*max(abs(array_max), abs(array_min));    
-    centered_array = array/scale + 0.5;    
+    if nargin == 1
+        data_min = min(data(:));
+        data_max = max(data(:));
+    elseif nargin == 2
+        cutoff = varargin{1};
+        data_min = prctile(data(:), cutoff);
+        data_max = prctile(data(:), 100 - cutoff);
+    else
+        error('Need to provide a data array!');
+    end
+
+    scale = 2.0*max(abs(data_max), abs(data_min));    
+    centered_array = data/scale + 0.5;    
 end
